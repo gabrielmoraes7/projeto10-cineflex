e26c824702bd1f42dec54dbf6e84578fac06e3e5
gabrielmoraes7 <gabriel.moclemente7@gmail.com>
2023-05-23 14:11:33 -0300

    seats page finalizada + Bonus do back button feito

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import HomePage from "./pages/HomePage/HomePage"
 import SeatsPage from "./pages/SeatsPage/SeatsPage"
 import SessionsPage from "./pages/SessionsPage/SessionsPage"
 import SuccessPage from "./pages/SuccessPage/SuccessPage"
-import { useState } from "react";
+import BackButton from "./components/BackButton"
 
 export default function App() {
   
 
     return (
         <>
-           <NavContainer>CINEFLEX</NavContainer>
+           
 
            <BrowserRouter>
+           <NavContainer>
+                <BackButton />
+                CINEFLEX
+            </NavContainer>
            <Routes>
               <Route exact path="/" element={<HomePage />} />
               <Route exact path="/sessoes/:idFilme" element={<SessionsPage />} />
@@ -30,19 +34,23 @@ export default function App() {
 }
 
 const NavContainer = styled.div`
-    width: 100%;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #C3CFD9;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #C3CFD9;
+  color: #E8833A;
+  font-family: 'Roboto', sans-serif;
+  font-size: 34px;
+  position: fixed;
+  top: 0;
+  a {
+    text-decoration: none;
     color: #E8833A;
-    font-family: 'Roboto', sans-serif;
-    font-size: 34px;
-    position: fixed;
-    top: 0;
-    a {
-        text-decoration: none;
-        color: #E8833A;
-    }
+  }
+  button {
+    position: absolute;
+    left: 10px;
+  }
 `
---
+++ src/components/BackButton.jsx
@@ -0,0 +1,22 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+
+export default function BackButton() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  if (location.pathname === '/') {
+    return null;
+  }
+  return (
+    <Btn><ion-icon name="arrow-back-outline" onClick={() => navigate(-1)}></ion-icon></Btn>
+  );
+}
+
+const Btn = styled.button`
+    display: flex;
+    left: 20px;
+    background: transparent;
+    color: Black;
+`;
+
